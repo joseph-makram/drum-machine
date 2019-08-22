7adc18fa11d2a6b41e3e1d4de3e7f4aa77f5da3c
jojomak13 <jojomak350@gmail.com>
2019-08-22 23:38:33 +0200

    Updates

---
+++ precache-manifest.f992d4782ef0d3e0ea1254bd1e862a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad844fb4ad113b341bc66f5d0b08f37d",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "adbac423a49f0212c44f",
+    "url": "/drum-machine/static/css/main.b7634f9f.chunk.css"
+  },
+  {
+    "revision": "7b47aa4b2be3dcb83135",
+    "url": "/drum-machine/static/js/2.9059c7c6.chunk.js"
+  },
+  {
+    "revision": "adbac423a49f0212c44f",
+    "url": "/drum-machine/static/js/main.7e2dabf8.chunk.js"
+  },
+  {
+    "revision": "9f4bdfe6e94080370121",
+    "url": "/drum-machine/static/js/runtime~main.0825f308.js"
+  }
+]);
